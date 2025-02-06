b76c16a20296b00999175fa5cbc9fc01ba3f081e
wlwu92 <wlwu.vipl@gmail.com>
2025-02-06 21:52:17 +0800

    Add pose_estimation util.

---
+++ pose_estimation/utils.py
@@ -0,0 +1,66 @@
+import json
+import numpy as np
+from PIL import Image, ImageDraw
+
+def load_pose(pose_path: str) -> np.ndarray:
+    """
+    Load a pose from a JSON file.
+
+    Args:
+        pose_path (str): The path to the JSON file containing the pose.
+
+    Returns:
+        np.ndarray: A numpy array containing the pose.
+    """
+    with open(pose_path, 'r') as file:
+        data = json.load(file)
+
+    assert len(data['instance_info']) == 1, "Only one instance is supported"
+    keypoints = data['instance_info'][0]['keypoints']
+    keypoint_scores = data['instance_info'][0]['keypoint_scores']
+    pose_array = np.array([kp + [score] for kp, score in zip(keypoints, keypoint_scores)])
+    return pose_array
+
+def to_openpose_format(pose_array: np.ndarray) -> np.ndarray:
+    """
+    Convert the pose array to the OpenPose format.
+    """
+    assert pose_array.shape[0] == 133, "Pose array must have 133 elements"
+    neck = np.mean(pose_array[[5, 6]], axis=0)
+    # neck score
+    neck[2] = np.logical_and(pose_array[5, 2] > 0.3, pose_array[6, 2] > 0.3).astype(int)
+    pose_array = np.insert(pose_array, 17, neck, axis=0)
+    mmpose_idx = [
+        17, 6, 8, 10, 7, 9, 12, 14, 16, 13, 15, 2, 1, 4, 3
+    ]
+    openpose_idx = [
+        1, 2, 3, 4, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17
+    ]
+    pose_array[openpose_idx] = pose_array[mmpose_idx]
+    return pose_array
+
+def get_pose_parts(pose_array: np.ndarray) -> dict:
+    """
+    Get the parts of the pose from the pose array.
+    """
+    body = pose_array[:18]
+    hands = pose_array[92:113]
+    hands = np.vstack([hands, pose_array[113:]])
+    faces = pose_array[24:92]
+    foot = pose_array[18:24]
+    return dict(body=body, hands=hands, faces=faces, foot=foot)
+
+
+def draw_pose_on_image(pose_array: np.ndarray, image: Image.Image, color: tuple | None = None) -> Image.Image:
+    """
+    Draw the pose on the image with color indicating score.
+    """
+    draw = ImageDraw.Draw(image)
+    for point in pose_array:
+        x, y, score = point
+        if color is None:
+            red = int(255 * score)
+            green = int(255 * (1 - score))
+            color = (red, green, 0)
+        draw.ellipse((x-2, y-2, x+2, y+2), fill=color, outline=color)
+    return image
---
+++ pose_estimation/utils_test.py
@@ -0,0 +1,57 @@
+import unittest
+import numpy as np
+from PIL import Image
+
+from pose_estimation.utils import (
+    load_pose, to_openpose_format, draw_pose_on_image, get_pose_parts
+)
+
+class TestLoadPose(unittest.TestCase):
+
+    def setUp(self):
+        self.test_pose_path = 'data/example_wholebody_pose.json'
+        self.test_image_path = 'data/example_wholebody_image.png'
+
+    def test_load_pose(self):
+        pose_array = load_pose(self.test_pose_path)
+        self.assertEqual(pose_array.shape[1], 3)
+        self.assertEqual(pose_array.shape[0], 133)
+        self.assertIsInstance(pose_array, np.ndarray)
+
+    def test_to_openpose_format(self):
+        pose_array = load_pose(self.test_pose_path)
+        openpose_format = to_openpose_format(pose_array)
+        self.assertEqual(openpose_format.shape[1], 3)
+        self.assertEqual(openpose_format.shape[0], 134)
+
+    def test_draw_pose(self):
+        pose_array = load_pose(self.test_pose_path)
+        image = Image.open(self.test_image_path)
+        draw_image0 = draw_pose_on_image(pose_array, image.copy())
+        draw_image0.save('data/example_wholebody_image_draw_pose.png')
+
+        openpose_format = to_openpose_format(pose_array)
+        draw_image1 = draw_pose_on_image(openpose_format, image.copy())
+        draw_image1.save('data/example_wholebody_image_draw_openpose.png')
+        self.assertIsInstance(image, Image.Image)
+
+    def test_get_pose_parts(self):
+        pose_array = load_pose(self.test_pose_path)
+        pose_array = to_openpose_format(pose_array)
+        pose_parts = get_pose_parts(pose_array)
+        self.assertIsInstance(pose_parts, dict)
+        self.assertEqual(len(pose_parts), 4)
+        self.assertEqual(pose_parts['body'].shape[0], 18)
+        self.assertEqual(pose_parts['hands'].shape[0], 42)
+        self.assertEqual(pose_parts['faces'].shape[0], 68)
+        self.assertEqual(pose_parts['foot'].shape[0], 6)
+
+        image = Image.open(self.test_image_path)
+        draw_image = draw_pose_on_image(pose_parts['body'], image, color=(255, 0, 0))
+        draw_image = draw_pose_on_image(pose_parts['hands'], image, color=(0, 255, 0))
+        draw_image = draw_pose_on_image(pose_parts['faces'], image, color=(0, 0, 255))
+        draw_image = draw_pose_on_image(pose_parts['foot'], image, color=(0, 255, 255))
+        draw_image.save('data/example_wholebody_image_draw_pose_parts.png')
+
+if __name__ == '__main__':
+    unittest.main()
