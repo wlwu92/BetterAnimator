f5addf69095a541d8cadb9c7085a0e469aa8359e
wlwu92 <wlwu.vipl@gmail.com>
2025-02-20 13:59:11 +0800

    Update preprocess_test.

--- modules/mimic_motion/preprocess_test.py
+++ modules/mimic_motion/preprocess_test.py
@@ -28,7 +28,7 @@ def generate_example_video_pose():
         pose = dwpose.detect(Image.fromarray(frame))
         save_pose(pose[0], f"data/mimicmotion_test/poses/{i}.json")
 
-def get_mimicmotion_alignment_result():
+def get_mimicmotion_alignment_result():    
     return [1.14420872, 1.14420872], [-0.0278025, -0.10101006]
     # add `return a, b` in mimicmotion.dwpose.preprocess.get_video_pose
     # from mimicmotion.dwpose.preprocess import get_video_pose
@@ -41,6 +41,8 @@ class PreprocessTest(unittest.TestCase):
     
     def setUp(self):
         self.expected_a, self.expected_b = get_mimicmotion_alignment_result()
+        self.ref_width, self.ref_height = Image.open(REF_IMAGE_PATH).size
+        self.video_width, self.video_height = 1080, 1920
         # generate_example_video_pose()
 
     def test_pose_alignment(self):
@@ -49,9 +51,22 @@ class PreprocessTest(unittest.TestCase):
         ref_pose_body = get_pose_parts(image_pose)['bodies']
         video_pose_bodies = [get_pose_parts(pose)['bodies'] for pose in video_poses]
         a, b = _pose_alignment(ref_pose_body, video_pose_bodies)
-        import pdb; pdb.set_trace()
-        np.testing.assert_allclose(a, self.expected_a)
-        np.testing.assert_allclose(b, self.expected_b)
+        # Normalized a/b to unormalized a/b
+        # (x / width * ax + bx) * dst_width -> x1
+        # (y / height * ay + by) * dst_height -> y1
+        # x * (ax / width * dst_width) + bx * dst_width -> x1
+        # y * (ay / height * dst_height) + by * dst_height -> y1
+        # ax / width * dst_width = ay / height * dst_height
+        dst_ay = self.expected_a[1] / self.video_height * self.ref_height
+        dst_ax = dst_ay
+        dst_by = self.expected_b[1] * self.ref_height
+        dst_bx = self.expected_b[0] * self.ref_width
+        print(a, b)
+        print(dst_ax, dst_ay, dst_bx, dst_by)
+        np.testing.assert_allclose(a[0], dst_ax, atol=1e-3)
+        np.testing.assert_allclose(a[1], dst_ay, atol=1e-3)
+        np.testing.assert_allclose(b[0], dst_bx, atol=1e0)
+        np.testing.assert_allclose(b[1], dst_by, atol=1e0)
 
     def tearDown(self):
         pass
